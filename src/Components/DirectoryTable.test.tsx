3bf27566fdeff21d3167a1b0961daa0b1a920d73
Angi Watt <angusjwatt@gmail.com>
2026-07-07 19:22:55 +0100

    adds headings to DirectoryTable

--- src/Components/DirectoryTable.test.tsx
+++ src/Components/DirectoryTable.test.tsx
@@ -1,7 +1,26 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import { Dir } from 'fs';
 import { DirectoryTable } from "./DirectoryTable";
 
 describe('DirectoryTable', () => {
-    it.todo('renders a table with headings for name, type, creation date, and a clickable link');
+    beforeEach(() => {
+        cleanup();
+    });
+
+    afterAll(() => {
+        cleanup();
+    });
+
+    it('renders a table with a caption, and headings for name, type, creation date, and a clickable link', () => {
+        render(<DirectoryTable caption="Test heading" />);
+        expect(screen.getByRole('caption')).toHaveTextContent('Test heading');
+        const headings = screen.getAllByRole('columnheader');
+        expect(headings.length).toBe(4);
+        expect(headings[0]).toHaveTextContent('Name');
+        expect(headings[1]).toHaveTextContent('Type');
+        expect(headings[2]).toHaveTextContent('Date');
+        expect(headings[3]).toHaveTextContent('Click to open');
+    });
 
     it.todo('renders rows with entries that correspond to the columns of the table');
 
--- src/Components/DirectoryTable.tsx
+++ src/Components/DirectoryTable.tsx
@@ -1,1 +1,17 @@
-export const DirectoryTable = () => {};
+type DirectoryTableProps = { caption: string; }
+
+export const DirectoryTable = ({ caption }: DirectoryTableProps) => {
+    return (
+        <table>
+            <caption>{caption}</caption>
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Click to open</th>
+                </tr>
+            </thead>
+        </table>
+    )
+};
