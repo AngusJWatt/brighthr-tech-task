c40b160b5349411f43fc967ce1c5a569849168d8
Angi Watt <angusjwatt@gmail.com>
2026-07-07 15:53:45 +0100

    updates list of test for CWDList

--- src/Components/CWDList.test.jsx
+++ src/Components/CWDList.test.jsx
@@ -2,8 +2,11 @@ import { render, screen } from '@testing-library/react'
 import { CWDList } from "./CWDList";
 
 describe('CWDList', () => {
-    it('renders a corresponding series of directories when provided with a current working directory list', () => {
-        render(<CWDList currentWorkingDirectory={['dir0', 'dir1']}/>);
-        expect(screen.getByText('HOME / dir0 / dir1'));
-    });
+    it.todo('renders a corresponding series of directories when provided with a current working directory list');
+
+    it.todo('renders only HOME when the list of directories is empty');
+
+    it.todo('renders all except the last directory as a clickable link');
+
+    it.todo('runs a callback with a new corresponding working directory list when a link is pressed');
 });
