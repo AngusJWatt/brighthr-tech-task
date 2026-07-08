e64debdfb8ba999a027f573bb1094c1d27e38a45
Angi Watt <angusjwatt@gmail.com>
2026-07-08 22:43:22 +0100

    refactors DirectoryTable and README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project was created as a junior-and-middleweight dev tech task by BrightHR,
 built according to the following specifications:\
-https://github.com/brighthr/Front-End-Tech-Tasks/blob/main/junior-and-middleweight.md.\
+https://github.com/brighthr/Front-End-Tech-Tasks/blob/main/junior-and-middleweight.md. \
 I pledge that all the work produced here is written entirely using my own
 knowledge and openly-available documentation, and made without the use of 
 generative AI.
--- src/Components/DirectoryTable.tsx
+++ src/Components/DirectoryTable.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 enum SortOrdering { Ascending = 'ascending', Descending = 'descending', Other = 'other' };
 
 type File = { nodeName: string; nodeType: string; added: Date; };
-type Directory = { nodeName: string; nodeType: string; files: (File | Directory)[]; added?: Date; };
+type Directory = { nodeName: string; nodeType: string; files: (File | Directory)[]; added?: undefined; };
 type FileNode = File | Directory;
 type DirectoryTableProps = {
     caption: string;
@@ -15,8 +15,23 @@ type DirectoryTableProps = {
 
 const filterNodes = (nodeList: FileNode[], filterRegex: RegExp):FileNode[] => {
     return nodeList.filter(({ nodeName, nodeType }) => {
-        const fullName = nodeType === 'folder' ? nodeName : `${nodeName}.${nodeType}`;
+        const fullName: string = nodeType === 'folder' ? nodeName : `${nodeName}.${nodeType}`;
         return filterRegex.test(fullName); 
+    });
+};
+const sortNames = (nodeArray: FileNode[], shouldInvert: boolean) => {
+    const signedFactor = shouldInvert ? -1 : 1;
+    return nodeArray.sort((nodeA, nodeB) => signedFactor * nodeA.nodeName.localeCompare(nodeB.nodeName));
+};
+const sortDates = (nodeArray: FileNode[], shouldInvert: boolean) => {
+    const signedFactor = shouldInvert ? -1 : 1;
+    return nodeArray.sort((nodeA, nodeB) => {
+        /* Value of signedFactor * Infinity keeps nodes without added dates at the bottom. Can be switched to 
+         * signedFactor * -Infinity if they should be at the top, or else removed entirely if this behaviour is
+         * undesirable. */
+        const nodeATime = nodeA.added?.getTime() || signedFactor * Infinity;
+        const nodeBTime = nodeB.added?.getTime() || signedFactor * Infinity;
+        return signedFactor * (nodeATime - nodeBTime || 0);
     });
 };
 
@@ -24,29 +39,17 @@ export const DirectoryTable = ({ caption, files, filterRegex, filePath, openDire
     const [nodesList, setNodesList] = useState(filterNodes(files, filterRegex));
     const [nameSort, setNameSort] = useState(SortOrdering.Other);
     const [dateSort, setDateSort] = useState(SortOrdering.Other);
-    const sortNames = () => {
-        if (nameSort !== SortOrdering.Ascending) {
-            setNodesList(prevList => prevList.sort((nodeA, nodeB) => nodeA.nodeName.localeCompare(nodeB.nodeName)));
-            setNameSort(SortOrdering.Ascending);
-        } else {
-            setNodesList(prevList => prevList.sort((nodeA, nodeB) => nodeB.nodeName.localeCompare(nodeA.nodeName)));
-            setNameSort(SortOrdering.Descending);
-        }
+    const updateNames = () => {
+        const shouldBecomeDescending = nameSort === SortOrdering.Ascending;
+        setNodesList(prevList => sortNames(prevList, shouldBecomeDescending));
+        setNameSort(shouldBecomeDescending ? SortOrdering.Descending : SortOrdering.Ascending);
         setDateSort(SortOrdering.Other);
     };
-    const sortDates = () => {
+    const updateDates = () => {
         /* Always put a filenode with a date above one without, but otherwise leave the order preserved. */
-        if (dateSort !== SortOrdering.Ascending) {
-            setNodesList(prevList => prevList.sort(
-                (nodeA, nodeB) => (nodeA.added?.getTime() || Infinity) - (nodeB.added?.getTime() || Infinity) || 0
-            ));
-            setDateSort(SortOrdering.Ascending);
-        } else {
-            setNodesList(prevList => prevList.sort(
-                (nodeA, nodeB) => (nodeB.added?.getTime() || -Infinity) - (nodeA.added?.getTime() || -Infinity) || 0
-            ));
-            setDateSort(SortOrdering.Descending);
-        }
+        const shouldBecomeDescending = dateSort === SortOrdering.Ascending;
+        setNodesList(prevList => sortDates(prevList, shouldBecomeDescending));
+        setDateSort(shouldBecomeDescending ? SortOrdering.Descending : SortOrdering.Ascending);
         setNameSort(SortOrdering.Other);
     };
 
@@ -54,27 +57,14 @@ export const DirectoryTable = ({ caption, files, filterRegex, filePath, openDire
         /* Cannot rely on previous state of nodesList, in case there is a subtraction applied to the regex, which means
          * applying a regex to a list with pertinent results already filtered out. Instead, take the files from props
          * and apply the regex to them, then sort them by the order the user set. */
-        const filterFiles = filterNodes(files, filterRegex);
-        /* TODO: Refactor sorting function to their own methods so they can be reused rather than
-         * duplicated/inverted. */
+        const filteredFiles = filterNodes(files, filterRegex);
         if (nameSort !== SortOrdering.Other) {
-            if (nameSort === SortOrdering.Ascending) {
-                filterFiles.sort((nodeA, nodeB) => nodeA.nodeName.localeCompare(nodeB.nodeName));
-            } else {
-                filterFiles.sort((nodeA, nodeB) => nodeB.nodeName.localeCompare(nodeA.nodeName));
-            }
-        } else if (dateSort !== SortOrdering.Other) {
-            if (dateSort === SortOrdering.Ascending) {
-                filterFiles.sort(
-                    (nodeA, nodeB) => (nodeA.added?.getTime() || Infinity) - (nodeB.added?.getTime() || Infinity) || 0
-                );
-            } else {
-                filterFiles.sort(
-                    (nodeA, nodeB) => (nodeB.added?.getTime() || -Infinity) - (nodeA.added?.getTime() || -Infinity) || 0
-                );
-            }
+            sortNames(filteredFiles, nameSort === SortOrdering.Descending);
         }
-        setNodesList(filterFiles);
+        if (dateSort !== SortOrdering.Other) {
+            sortDates(filteredFiles, dateSort === SortOrdering.Descending);
+        }
+        setNodesList(filteredFiles);
     }, [files, filterRegex]);
 
     return (
@@ -83,11 +73,11 @@ export const DirectoryTable = ({ caption, files, filterRegex, filePath, openDire
             <thead>
                 <tr>
                     <th scope="col" aria-sort={nameSort}>
-                        <button onClick={sortNames}>Name (Click to sort)</button>
+                        <button onClick={updateNames}>Name (Click to sort)</button>
                     </th>
                     <th scope="col">Type</th>
                     <th scope="col" aria-sort={dateSort}>
-                        <button onClick={sortDates}>Date added (Click to sort)</button>
+                        <button onClick={updateDates}>Date added (Click to sort)</button>
                     </th>
                     <th scope="col">Link</th>
                 </tr>
