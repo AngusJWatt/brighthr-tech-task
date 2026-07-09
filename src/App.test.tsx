5d72d6ac07c88cca28166bed44bd66205bea2074
Angi Watt <angusjwatt@gmail.com>
2026-07-09 18:10:00 +0100

    adds CWDList to App, connects links to update current working directory

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen, waitFor, cleanup } from '@testing-library/react';
+import { render, screen, waitFor, within, cleanup } from '@testing-library/react';
 import * as getFilesModule from './functions/getFiles';
 
 import App from './App';
 import { FileNode } from './types';
+import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
   beforeEach(() => {
@@ -26,11 +27,54 @@ describe('App', () => {
     render(<App />);
 
     await waitFor(() => {
-      expect(screen.getAllByRole('row').length).toBe(3);
+      const rows = screen.getAllByRole('row');
+      expect(rows.length).toBe(3);
+      expect(within(rows[1]).getByRole('rowheader')).toHaveTextContent('Christmas party');
+      expect(within(rows[2]).getByRole('rowheader')).toHaveTextContent('Greetings');
     });
   });
 
-  it.todo('renders the table corresponding to a directory when its button is clicked');
+  it('renders the table and current path corresponding to a directory when its button is clicked', async () => {
+    jest.spyOn(getFilesModule, 'getFiles').mockImplementation((): Promise<FileNode[]> => 
+      Promise.resolve([
+        { type: 'folder', name: 'Greetings', files: [
+          { type: 'mov', name: 'Welcome to the company!', added: '2015-04-24' }
+        ]}
+      ])
+    );
+    render(<App />);
+    let cwdList = screen.getByTestId('cwd-list');
+    let rows: HTMLElement[] = [];
+
+    expect(cwdList).toHaveTextContent('HOME');
+
+    await waitFor(() => {
+      cwdList = screen.getByTestId('cwd-list');
+      rows = screen.getAllByRole('row');
+
+      expect(within(rows[1]).getByRole('rowheader')).toHaveTextContent('Greetings');
+    });
+
+    userEvent.click(within(rows[1]).getByRole('button'));
+
+    await waitFor(() => {
+      cwdList = screen.getByTestId('cwd-list');
+      rows = screen.getAllByRole('row');
+
+      expect(cwdList).toHaveTextContent('HOME / Greetings');
+      expect(within(rows[1]).getByRole('rowheader')).toHaveTextContent('Welcome to the company!');
+    });
+
+    userEvent.click(within(cwdList).getByRole('button'));
+
+    await waitFor(() => {
+      cwdList = screen.getByTestId('cwd-list');
+      rows = screen.getAllByRole('row');
+
+      expect(cwdList).toHaveTextContent('HOME');
+      expect(within(rows[1]).getByRole('rowheader')).toHaveTextContent('Greetings');
+    });
+  });
 
   it.todo('clears the search bar after clicking a new directory link, ');
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
+import { CWDList } from './components/CWDList';
 import { SearchAndTable } from './components/SearchAndTable';
 import { getFiles } from './functions/getFiles';
-import type { FileNode } from './types';
+import type { Directory, FileNode } from './types';
 
 function App() {
+  const allFiles = useRef([] as FileNode[]);
+  const [cwdFilePath, setCWDFilePath] = useState([] as string[]);
   const [cwdFiles, setCWDFiles] = useState([] as FileNode[]);
 
   useEffect(() => {
     getFiles().then(files => {
+      allFiles.current = files;
       setCWDFiles(files);
     }).catch(e => console.error(e));
   }, []);
+
+  useEffect(() => {
+    const updatedDirContents = cwdFilePath.reduce((acc, val) => {
+      const dirIndex = acc.findIndex(filenode => filenode.type === 'folder' && filenode.name === val)
+      return (acc[dirIndex] as Directory).files;
+    }, allFiles.current)
+    setCWDFiles(updatedDirContents);
+  }, [cwdFilePath]);
 
   return (
     <div className="App">
       <h1>Angi Watt's BrightHR File Reader</h1>
       <section>
-        <SearchAndTable tableCaption="" filePath={[]} files={cwdFiles} openDirectory={() => {}}/>
+        <CWDList currentWorkingDirectory={cwdFilePath} setCurrentWorkingDirectory={setCWDFilePath} />
+        <SearchAndTable tableCaption="" filePath={[]} files={cwdFiles} openDirectory={setCWDFilePath}/>
       </section>
     </div>
   );
