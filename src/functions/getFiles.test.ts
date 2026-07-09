5856464fa8fd378ee64b4d5b6e7e2e84251be311
Angi Watt <angusjwatt@gmail.com>
2026-07-09 09:57:21 +0100

    creates basic fetch functionality for getFiles

--- src/functions/getFiles.test.ts
+++ src/functions/getFiles.test.ts
@@ -1,5 +1,37 @@
-describe('getFiles', () => {
-    it.todo('maps files and folders recursively');
+import { cleanup } from '@testing-library/react';
+import { getFiles } from "./getFiles";
 
-    it.todo('removes any directory that does not have a correctly-formatted files property');
+const mockData = [
+    { type: "csv", name: "Cost centres", added: "2016-08-12" },
+    { type: "folder", name: "Misc", files: [
+        { type: "doc", name: "Christmas party", added: "2017-12-02" },
+        { type: "mov", name: "Welcome to the company!", added: "2015-04-24" },
+        { type: "folder", name: "Empty", files: [] }
+    ]
+}];
+
+describe('getFiles', () => {
+    beforeEach(() => {
+        cleanup();
+    });
+
+    afterAll(() => {
+        jest.restoreAllMocks();
+        cleanup();
+    });
+
+    it('formats files and folders recursively', async () => {
+        jest.spyOn(global, 'fetch').mockImplementation((): any =>
+            Promise.resolve({ json: () => Promise.resolve(JSON.stringify(mockData)) })
+        );
+
+        const data = await getFiles();
+        expect(data).toStrictEqual(mockData);
+    });
+
+    it.todo('throws an error if the response is not correctly handled');
+
+    it.todo('removes any directory that does not have a name or correctly-formatted files property');
+
+    it.todo('removes any directory that does not have a name');
 });
--- src/functions/getFiles.ts
+++ src/functions/getFiles.ts
@@ -1,3 +1,7 @@
-export const getFiles = () => {
-    
+import type { FileNode } from "../types";
+
+export const getFiles = async (): Promise<FileNode[]> => {
+    const response = await fetch('../../public/fetch');
+    const data = await response.json();
+    return JSON.parse(data) as FileNode[];
 };
