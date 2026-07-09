8d5caf12966f59a85b3aa2b5824868954d04f243
Angi Watt <angusjwatt@gmail.com>
2026-07-09 16:47:43 +0100

    adds test for fetching and displaying data in the App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, cleanup } from '@testing-library/react';
+import * as getFilesModule from './functions/getFiles';
 
 import App from './App';
+import { FileNode } from './types';
 
 describe('App', () => {
-  it.todo('fetches data from the mocked API and populates the table with its first layer of files');
+  beforeEach(() => {
+        cleanup();
+    });
+
+    afterAll(() => {
+        jest.restoreAllMocks();
+        cleanup();
+    });
+
+  it('fetches data from the mocked API and populates the table with its first layer of files', async () => {
+    jest.spyOn(getFilesModule, 'getFiles').mockImplementation((): Promise<FileNode[]> => 
+      Promise.resolve([
+        { type: 'doc', name: 'Christmas party', added: '2017-12-02' },
+        { type: 'folder', name: 'Greetings', files: [
+          { type: 'mov', name: 'Welcome to the company!', added: '2015-04-24' }
+        ]}
+      ])
+    );
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getAllByRole('row').length).toBe(3);
+    });
+  });
 
   it.todo('renders the table corresponding to a directory when its button is clicked');
 
